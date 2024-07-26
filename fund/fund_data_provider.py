3f3999cf4f2c19c33a48580439e8e8dbaa32972e
wangqiang <wangq@yusur.tech>
2024-07-26 08:42:06 +0800

    ✨ feat(fund): more fields

--- fund/fund_data_provider.py
+++ fund/fund_data_provider.py
@@ -1,4 +1,5 @@
 import akshare as ak
+import pandas as pd
 
 class FundDataProvider(object):
     """
@@ -18,7 +19,21 @@ class FundDataProvider(object):
 class AkshareFundDataProvider(FundDataProvider):
 
     def get_fund_data(self):
+        # 获取基金基本信息
         fund_name_em_df = ak.fund_name_em()
+        # 获取基金申购状态
+        fund_purchase_em_df = ak.fund_purchase_em()
+        # 按照基金代码合并，并只保留两个 DataFrame 中都有的基金代码
+        merged_df = pd.merge(fund_name_em_df, fund_purchase_em_df, on="基金代码", how="inner", suffixes=('', '_duplicate'))
+
+        # 删除所有带有 '_duplicate' 后缀的列
+        merged_df = merged_df.loc[:, ~merged_df.columns.str.endswith('_duplicate')]
+
+
+        print(merged_df)
+        html = merged_df.to_html()
+        with open('merged_dataframe.html', 'w') as f:
+            f.write(html)
         # 替换列名
         fund_name_em_df.rename(
             columns={
@@ -30,14 +45,15 @@ class AkshareFundDataProvider(FundDataProvider):
             },
             inplace=True,
         )
-        # 将 DataFrame 转换为字典列表
         return fund_name_em_df.to_dict(orient="records")
 
 
 if __name__ == "__main__":
     fund_data_provider = AkshareFundDataProvider()
     fund_data = fund_data_provider.get_fund_data()
-    print(fund_data[1])
+    # print(fund_data)
+    fund_purchase_em_df = ak.fund_purchase_em()
+    # print(fund_purchase_em_df)
     for item in fund_data:
         try:
             pass
